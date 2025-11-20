997a076ef3619d1a5fcc2dc25db0c20f0bd2e2f5
mui1233 <abrorerkinov18@gmail.com>
2025-11-20 00:07:31 -0500

    Create inputs and outputs in main.py.

---
+++ main.py
@@ -0,0 +1,25 @@
+
+inputs = [(0, 0), (1, 0), (0, 1), (1, 1)] # All possible pairs of inputs
+outputs = [0, 1, 1, 0] # Corresponding correct outputs
+inputs_outputs = {inputs[i]: outputs[i] for i in range(len(inputs))} # Create a disctionary
+
+print(inputs_outputs)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
